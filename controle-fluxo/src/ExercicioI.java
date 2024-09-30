726142009048fb6559938cec2cee419a714ede92
lolnico <viniciusreispinto.ifpr@gmail.com>
2024-09-30 15:31:30 -0300

    Exericicios

---
+++ controle-fluxo/src/ExercicioI.java
@@ -0,0 +1,9 @@
+public class ExercicioI {
+    public static void main(String[] args) throws Exception {
+        int idade = 14;
+        if (idade >= 16)
+            System.out.println("Vá para a piscina de adultos");
+        else
+            System.out.println("Vá para a piscina de de crianças");
+    }
+}
---
+++ controle-fluxo/src/ExercicioII.java
@@ -0,0 +1,11 @@
+public class ExercicioII {
+    public class App {
+        public static void main(String[] args) throws Exception {
+            int idade = 14;
+            if (idade >= 16)
+                System.out.println("Vá para a piscina de adultos");
+            else
+                System.out.println("Vá para a piscina de de crianças");
+        }
+    }
+}
