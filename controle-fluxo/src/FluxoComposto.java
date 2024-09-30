2c446cc4b003dd636a8b3447eb62bf32292e7434
lolnico <viniciusreispinto.ifpr@gmail.com>
2024-09-30 15:20:43 -0300

    Fluxos-e-PlanoOperadora

---
+++ controle-fluxo/src/FluxoComposto.java
@@ -0,0 +1,14 @@
+public class FluxoComposto {
+    public static void main(String[] args) {
+
+        //fluxo composto
+        
+        int nota = 6;
+
+        if (nota >= 7) 
+            System.out.println("Aprovado");
+        
+        else
+            System.out.println("Reprovado");
+    }
+}
